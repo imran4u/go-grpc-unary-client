1c51d7e0ee31956c6a01b9f2cc8f3064cb7352fa
imran4u <er.imran4u@gmail.com>
2024-11-08 07:13:40 +0530

    Add main .go

--- cmd/main.go
+++ cmd/main.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	"errors"
-
+	"github.com/imran4u/go-grpc-unary-client/internal/adapter/hello"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
-
-
 func main() {
- var opts []grpc.DialOption
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
- opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials())
+	conn, err := grpc.NewClient("localhost:8080", opts...)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
 
- conn, err := grpc.NewClient("localhost:5050", opts...)
- if err != nil {	
-	panic(err)	
- }
- defer conn.Close()
+	helloAdapter, err := hello.NewHelloAdapter(conn)
+	if err != nil {
+		panic(err)
+	}
 
- helloAdapter, err := mygrpc.NewHelloAdapter(conn)
- if err != nil {	
-	panic(err)	
- }	
-
- name := "imran"
- println(helloAdapter.SayHello(name))	
+	name := "imran"
+	println(helloAdapter.SayHello(name))
 }
